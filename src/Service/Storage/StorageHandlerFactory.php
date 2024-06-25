28fbb786933197b661f382bdec3af17a6bb66049
siergiej <s.kulinicz@gmail.com>
2024-06-25 17:46:03 +0200

    const create

--- src/Service/Storage/StorageHandlerFactory.php
+++ src/Service/Storage/StorageHandlerFactory.php
@@ -6,6 +6,9 @@ use Aws\S3\S3Client;
 
 class StorageHandlerFactory
 {
+    const TYPE_LOCAL = 'local';
+    const TYPE_S3 = 's3';
+
     private S3Client $s3Client;
 
     public function __construct(S3Client $s3Client)
@@ -15,9 +18,9 @@ class StorageHandlerFactory
 
     public function create(string $type, ?string $bucketName = null): StorageHandlerInterface
     {
-        if ($type === 'local') {
+        if ($type === self::TYPE_LOCAL) {
             return new LocalStorageHandler();
-        } elseif ($type === 's3' && $bucketName) {
+        } elseif ($type === self::TYPE_S3 && $bucketName) {
             return new S3StorageHandler($this->s3Client, $bucketName);
         } else {
             throw new \InvalidArgumentException('Invalid storage type or missing bucket name.');
